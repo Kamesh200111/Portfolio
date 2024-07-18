68deaf6b5762b84d930f31b731d521bb347c5d6d
Kameshwaran <kameshwaran@markplace.in>
2024-07-18 12:07:56 +0530

    first commit

---
+++ public/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Backend Developer", "Java Developer", ".Net Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
